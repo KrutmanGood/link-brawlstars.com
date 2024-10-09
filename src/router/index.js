eb8fd54e40ffcef6d91ffafcc9b6245fd55b3852
KrutmanGood <83672816+KrutmanGood@users.noreply.github.com>
2024-10-09 17:47:11 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const router = createRouter({
 
     {
       path: '/en/?action=voucher&code=3000dcf5-b25d-467e-a4d3-cf597a5f6a23',
-      name: 'news',
+      name: 'aksela',
       component: () => import('@/pages/AkselaPage.vue'),
     },
   ]
